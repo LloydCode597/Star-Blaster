6c6afa9caa39d7a2c375133cedac9f1f4929f728
Lloyd Allen <lloydallen597@gmail.com>
2026-01-25 00:29:41 -0600

    Add player movement bounds with padding
    Introduces configurable padding for player movement boundaries in PlayerController. The player's position is now clamped within the camera's viewport, preventing movement outside the visible area.

--- Star Blaster/Assets/Scripts/PlayerController.cs
+++ Star Blaster/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,14 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float leftBoundPadding;
+    [SerializeField] float rightBoundPadding;
+    [SerializeField] float upBoundPadding;
+    [SerializeField] float downBoundPadding;
+
 
     InputAction moveAction;
 
     Vector3 moveVector;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+
+        InitBounds();
     }
 
     void Update()
@@ -20,11 +31,21 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+    }
+
     void MovePlayer()
     {
         moveVector = moveAction.ReadValue<Vector2>();
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime;
 
-        transform.position += moveVector * moveSpeed * Time.deltaTime;
+        newPos.x = Math.Clamp(newPos.x, minBounds.x + leftBoundPadding, maxBounds.x - rightBoundPadding);
+        newPos.y = Math.Clamp(newPos.y, minBounds.y + downBoundPadding, maxBounds.y - upBoundPadding);
+        transform.position = newPos;
     }
 
 }
